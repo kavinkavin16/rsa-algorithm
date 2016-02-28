dec531429b81485f74feadc2fc8e50c52fd49ccc
Liuri Loami <liuriloami@gmail.com>
2016-02-28 12:31:49 +0000

    BigInteger prototype implemented

---
+++ src/lib/BigInteger.js
@@ -0,0 +1,39 @@
+var BigInteger = BigInteger(value, sign) {
+
+};
+
+BigInteger.prototype.add = function(n) {
+
+};
+
+BigInteger.prototype.minus = function(n) {
+
+};
+
+BigInteger.prototype.times = function(n) {
+
+};
+
+BigInteger.prototype.mod = function(n) {
+
+};
+
+BigInteger.prototype.pow = function(n) {
+
+};
+
+BigInteger.prototype.eq = function(n) {
+
+};
+
+BigInteger.prototype.abs = function() {
+
+};
+
+BigInteger.prototype.square = function() {
+
+};
+
+BigInteger.prototype.toString = function() {
+
+};
