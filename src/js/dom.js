0e65fb7f8faa8797dbdb9311ba49a9396680b683
Liuri Loami <liuriloami@gmail.com>
2016-02-28 12:18:20 +0000

    Using CDN for third-party Javascript files; Unused files removed.

--- src/js/dom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(document).on('click', ':not(.dropdown *)', function (e) {
-        e.stopPropagation();
-        $('#menu-dropdown').removeClass("open");
-    });
-    closemodal = function(modal) {
-        $(modal).modal('hide')
-    };
-});
